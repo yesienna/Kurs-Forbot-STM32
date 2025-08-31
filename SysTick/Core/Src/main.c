3601c02c87652f6821cedbb32fff393109f44cdd
yesienna <sunvihowell@wp.pl>
2025-08-31 14:38:56 +0200

    SysTick HAL_UART_Receive_IT LD2

--- SysTick/Core/Src/main.c
+++ SysTick/Core/Src/main.c
@@ -120,6 +120,53 @@ void send_next_message(void)
   }
 }
 
+#define LINE_MAX_LENGTH 80
+static char line_buffer[LINE_MAX_LENGTH + 1];
+static uint32_t line_length;
+void line_append(uint8_t value)
+{
+  if (value == '\r' || value == '\n') {
+    // odebraliśmy znak końca linii
+    if (line_length > 0) {
+      // dodajemy 0 na końcu linii
+      line_buffer[line_length] = '\0';
+      // przetwarzamy dane
+      if (strcmp(line_buffer, "on") == 0) {
+        HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+      } else if (strcmp(line_buffer, "off") == 0) {
+        HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+      }
+      // zaczynamy zbieranie danych od nowa
+      line_length = 0;
+    }
+  }
+  else {
+    if (line_length >= LINE_MAX_LENGTH) {
+      // za dużo danych, usuwamy wszystko co odebraliśmy dotychczas
+      line_length = 0;
+    }
+    // dopisujemy wartość do bufora
+    line_buffer[line_length++] = value;
+  }
+}
+
+
+uint8_t uart_rx_buffer;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+
+{
+
+  if (huart == &huart2) {
+
+    line_append(uart_rx_buffer);
+
+    HAL_UART_Receive_IT(&huart2, &uart_rx_buffer, 1);
+
+  }
+
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -163,6 +210,9 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   uint32_t old_push_counter = push_counter;
+
+  HAL_UART_Receive_IT(&huart2, &uart_rx_buffer, 1);
+
   while (1)
   {
 	//    printf("systick = %lu\n", HAL_GetTick());
--- SysTick/Core/Src/stm32l4xx_it.c
+++ SysTick/Core/Src/stm32l4xx_it.c
@@ -185,11 +185,12 @@ void SysTick_Handler(void)
   /* USER CODE BEGIN SysTick_IRQn 0 */
 	static int clk_div;
 	  clk_div++;
-
+/*
 	  if (clk_div >= 100) {
 	    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 	    clk_div = 0;
 	  }
+*/
   /* USER CODE END SysTick_IRQn 0 */
   HAL_IncTick();
   /* USER CODE BEGIN SysTick_IRQn 1 */
