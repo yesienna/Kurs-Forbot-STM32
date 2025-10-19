7bf0739998cdcc6212d4e22af87cebc05676fee5
yesienna <sunvihowell@wp.pl>
2025-10-19 13:44:28 +0200

    ADC potentiometer

--- ADC/Core/Src/main.c
+++ ADC/Core/Src/main.c
@@ -229,7 +229,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_640CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
   sConfig.OffsetNumber = ADC_OFFSET_NONE;
   sConfig.Offset = 0;
